0904907c6c5a9d625d2518950e0a415ded16ba6e
Tyrone <tvalverdes9@gmail.com>
2024-01-04 23:56:37 -0500

    feature: house details added

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,22 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms' 
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      details works! {{ housingLocationId }}
-    </p>
-  `,
+  imports: [ReactiveFormsModule],
+  templateUrl: './details.template.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocation = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocation);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
